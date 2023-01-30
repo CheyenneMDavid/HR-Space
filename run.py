5a041e512a7e480398f3691971606208fa8ec1d9
CheyenneMDavid <ctenmanikdavid@gmail.com>
2023-01-30 03:58:34 +0000

    Creat function to request task selection

--- run.py
+++ run.py
@@ -13,8 +13,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("hr_space")
 
-sheet_1 = SHEET.worksheet("Sheet1")
 
-data = sheet_1.get_all_values()
+def task():
+    """
+    Request user to choose from two different tasks.
+    1st task is to register a new employee
+    2nd task is record an employees sick absence
+    """
+    print("To record new staff details, please select '1'")
+    print("For staff attendance records, please select '2' \n")
 
-print(data)
+    choice = int(input("Please make your selection: \n"))
+    if choice == 1:
+        print("Place holder to run: new_staff function")
+    elif choice == 2:
+        print("Place holder to run: attendance_records function")
+    else:
+        print("To record new staff details, please select '1' and press 'ENTER")
+        print("For staff attendance records, please select '2' and press 'ENTER' \n")
+
+
+task()
